f02389d1fc1ce33b5dc53bdb7ebf74f04c712d8d
stormdirus2 <stormdirus2@protonmail.com>
2021-04-14 08:56:54 -0600

    Remove unused mixins

--- src/main/java/net/fenrir/mementomori/MementoMori_CLIENT.java
+++ src/main/java/net/fenrir/mementomori/MementoMori_CLIENT.java
@@ -2,6 +2,10 @@ package net.fenrir.mementomori;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.util.Identifier;
 
 public class MementoMori_CLIENT implements ClientModInitializer {
@@ -16,6 +20,17 @@ public class MementoMori_CLIENT implements ClientModInitializer {
         });
         ClientSidePacketRegistry.INSTANCE.register(new Identifier("mementomori:attrition_growth"), (packetContext, packetByteBuf) -> {
             attritionGrowth = packetByteBuf.readBoolean();
+        });
+        FabricModelPredicateProviderRegistry.register(MementoMori.REPLACE_BOOK, (stack, world, entity) -> {
+            ListTag enchantments = EnchantedBookItem.getEnchantmentTag(stack);
+            for (int i = 0; i < enchantments.size(); i++) {
+                CompoundTag tag = enchantments.getCompound(i);
+                Identifier enchantId = Identifier.tryParse(tag.getString("id"));
+                if (enchantId != null && (enchantId.equals(MementoMori.reaping) || enchantId.equals(MementoMori.soulCleaving))) {
+                    return tag.getInt("lvl");
+                }
+            }
+            return 0F;
         });
     }
 }
