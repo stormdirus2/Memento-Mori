589a664a9c639cdc19e202d6fdbc03e161d059e5
stormdirus2 <stormdirus2@protonmail.com>
2021-05-04 10:14:42 -0600

    * Fix spider eyes not dropping correctly for servers. * Fixes phantoms attacking while the player is in sufficient light levels.

--- src/main/java/net/fenrir/mementomori/mixin/PhantomEntityMixin.java
+++ src/main/java/net/fenrir/mementomori/mixin/PhantomEntityMixin.java
@@ -1,11 +1,13 @@
 package net.fenrir.mementomori.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PhantomEntity.class)
@@ -18,20 +20,33 @@ public abstract class PhantomEntityMixin extends MobEntityMixin {
         super(entityType, world);
     }
 
-    @Override
-    public void retrieve(CallbackInfoReturnable<LivingEntity> cir) {
+    private boolean checkLight(Entity target) {
         if (takeBreak) {
-            LivingEntity target = cir.getReturnValue();
             if (target == null || world.getLightLevel(target.getBlockPos()) > 4) {
-                cir.setReturnValue(null);
-                return;
+                return false;
             } else {
                 takeBreak = false;
             }
         }
         if (world.getLightLevel(this.getBlockPos()) > 4) {
             takeBreak = true;
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void retrieve(CallbackInfoReturnable<LivingEntity> cir) {
+        LivingEntity target = cir.getReturnValue();
+        if (!checkLight(target)) {
             cir.setReturnValue(null);
+        }
+    }
+
+    @Override
+    public void tryAttacking(Entity target, CallbackInfoReturnable<Boolean> cir) {
+        if (!checkLight(target) || world.getLightLevel(target.getBlockPos()) > 4) {
+            cir.cancel();
         }
     }
 
